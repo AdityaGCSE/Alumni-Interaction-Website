19a7c21cb7ac5982738b1ee016b0e1289628742f
Aditya Ghosh <104888138+AdityaGCSE@users.noreply.github.com>
2023-01-09 23:27:25 +0530

    Update User.js

--- frontend/src/Components/User/User.js
+++ frontend/src/Components/User/User.js
@@ -6,10 +6,26 @@ import {BiUser} from 'react-icons/bi'
 import {BiUserCircle} from 'react-icons/bi'
 import './User.css'
 
+const API_BASE = 'http://localhost:3000/api/auth';
+
 export default function User() {
     const {user,logout,isAuthenticated} = useAuth0();
     if(isAuthenticated)
     {   
+          fetch(API_BASE + "/create", {  
+          method: 'POST',
+          headers: {
+            "Content-Type" : "application/json"
+          },
+          body: JSON.stringify({
+            name: user.name,
+            email: user.email,
+            password: "enter password",
+            degree: "B.Tech",
+            graduation_year: "1900"
+          })
+        })
+
         return (
             <div>
             <NavDropdown title={<> <BiUserCircle/> <span id='username'>welcome {user.name} !</span> </>  } id="basic-nav-dropdown"  className='user'>
