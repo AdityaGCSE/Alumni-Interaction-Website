2681cace0f0bab1067239f4c46b11341ba64f1f1
Aditya Ghosh <104888138+AdityaGCSE@users.noreply.github.com>
2023-01-09 23:28:07 +0530

    Update Userinfo.js

--- frontend/src/Components/User/Userinfo.js
+++ frontend/src/Components/User/Userinfo.js
@@ -1,116 +1,106 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Userinfo.css'
 import userimg from '../../images/Aditya_Ghosh_profile.png'
 import { useAuth0 } from '@auth0/auth0-react'
 
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 
-const API_BASE = "http://localhost:3000/database"
+const API_BASE = "http://localhost:3000/api/auth"
 
 export default function Userinfo() {
-  const [name,setName] = useState(" Aditya Ghosh");
+
+  const {user,isAuthenticated} = useAuth0();
+
+  const [User, setUser ] = useState({});
+  
+
   const[namesize,setNameSize] = useState(50);
-  const [email,setEmail] = useState("adityag30802@gmail.com");
   const[emailsize,setEmailSize] = useState(50);
-  const [gradyear,setGradyear] = useState("2024");
   const[gradyearsize,setGradyearSize] = useState(50);
-  const [deg,setDeg] = useState("B.Tech");
   const[degsize,setDegSize] = useState(50);
-  const [about,setAbout] = useState("Software Development Engineer @Google");
-  const[aboutsize,setAboutSize] = useState(50);
   const [readonly,changeReadonly] = useState(true);
   const[status,setStatus] = useState('Edit Profile');
   const[inputbox,setinputbox] = useState('noneditable');
 
-  function onbuttonclick () {
-      changeReadonly(!readonly);
-      if(status==='Edit Profile')
-      {
-        setinputbox('editable');
-        setNameSize(20);
-        setEmailSize(20);
-        setGradyearSize(20);
-        setDegSize(20);
-        setAboutSize(20);
-        setStatus('Save');
-      }
-      else
-      {
-        setinputbox('noneditable');
-        setNameSize(name.length);
-        setEmailSize(email.length);
-        setGradyearSize(gradyear.length);
-        setDegSize(deg.length);
-        setAboutSize(about.length);
-        changeTodo();
-        setStatus('Edit Profile');
-      }
-  }
 
-  const {user,isAuthenticated} = useAuth0();
-
-
-  const[todos,setTodos] = useState([]);
-
-  console.log(todos);
-
-  const GetTodos = () => {
-    fetch(API_BASE).then(res => res.json()).then(data => setTodos(data));
+  const fetchData = () => {
+    if(isAuthenticated  && status!=='Save')
+    return fetch(API_BASE).then(res => res.json()).then(data=> data.filter(usr=> usr.email===user.email)).then(usr=> setUser(usr[0]));
   }
 
   useEffect(()=>{
-    GetTodos();
-  },[]);
+    if(isAuthenticated   && status!=='Save')
+    fetchData();
+  });
 
-  const changeTodo = async id => {
-    const data = await fetch(API_BASE + id, {
-      method: "GET",
+  function changeUserinfo(){
+    fetch(API_BASE + "/update", {  
+      method: 'PUT',
       headers: {
         "Content-Type" : "application/json"
       },
       body: JSON.stringify({
-        name: name
+        name: user.name,
+        email: user.email,
+        password: "password#123",
+        degree: User.degree,
+        graduation_year: User.graduation_year
       })
-    }).then(res => res.json());
-    setTodos([...todos,data]);
-    // setPop
+    })
   }
+
+  function onbuttonclick () {
+      changeReadonly(!readonly);
+      setEmailSize(User.email.length);
+      if(status==='Edit Profile')
+      {
+        setinputbox('editable');
+        setNameSize(20);
+        setGradyearSize(20);
+        setDegSize(20);
+        setStatus('Save');
+      }
+      else
+      {
+        window.location.reload(true);
+        setinputbox('noneditable');
+        setNameSize(User.name.length);
+        setGradyearSize(User.graduation_year.length);
+        setDegSize(User.degree.length);
+        changeUserinfo();
+        setStatus('Edit Profile');
+      }
+  }
+
 
 
   if(isAuthenticated){
   return (
-    <div className='userinfo'>
-    
-      {todos.map(todo=>(
-      <div  key={todo._id} >
+    <div className='userinfo' key = {user.email}>
         <div className="userprof">
           <img src={userimg} alt="uesrimg" className='userprofimg' />
-          {todo.name}
+          {user.name} <br />
           NIT DURGAPUR
         </div>
         <div className="userinfdiv">
         <div className="info">
-          <b>Name:</b>  <input className={inputbox} type='text' value={name} onChange={e =>setName(e.target.value)} readOnly={readonly} size={namesize}/> 
+          <b>Name:</b>  <input className={inputbox} id='nm' value={User.name}  readOnly= 'true' size={namesize}/> 
         </div>
         <div className="info">
-          <b>Email ID:</b>  <input className={inputbox} type='text' value={email}  onChange={e =>setEmail(e.target.value)} readOnly={readonly} size={emailsize}/>
+          <b>Email ID:</b>  <input className={inputbox} id='em' type='text' value={User.email}   readOnly = 'true' size={emailsize}/>
         </div>
         <div className="info">
-          <b>Graduation Year:</b> <input className={inputbox} type='text' value={gradyear} onChange={e =>setGradyear(e.target.value)} readOnly={readonly} size={gradyearsize}/> 
+          <b>Graduation Year:</b> <input className={inputbox} type='text' value={User.graduation_year} onChange={e =>setUser(prevstate => {return{ ...prevstate, graduation_year : e.target.value}} )} readOnly={readonly} size={gradyearsize}/> 
         </div>
         <div className="info">
-          <b>Degree:</b> <input className={inputbox} type='text' value={deg}  onChange={e =>setDeg(e.target.value)} readOnly={readonly} size={degsize}/>
+          <b>Degree:</b> <input className={inputbox} type='text' value={User.degree}  onChange={e =>setUser(prevstate => {return{ ...prevstate, degree : e.target.value}} )} readOnly={readonly} size={degsize}/>
         </div>
-        <div className="info">
+        {/* <div className="info">
           <b>About:</b> <input className={inputbox} type='text' value={about}  onChange={e =>setAbout(e.target.value)} readOnly={readonly} size={aboutsize}/> 
-        </div>
+        </div> */}
         <button className='edit' onClick={()=> onbuttonclick()}>{status}</button>
       </div>
       </div>
-      ))}
-      
-  
-    </div>
   )
   }
 }
